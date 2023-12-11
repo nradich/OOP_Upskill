f156b3dee045896ea6fe89e435348dc3aca71e24
avanickradich <nicholasradich1@gmail.com>
2023-12-11 15:40:19 -0800

    method to calculate point distance

--- Geometry/geometry.py
+++ Geometry/geometry.py
@@ -4,13 +4,20 @@ class Point:
         self.x =x
         self.y =y
 
-    def fall_in_rectangle(self, lowleft:int, upright:int):
+    def fall_in_rectangle(self, lowleft:list, upright:list) -> bool:
         """Takes an input and compares vs the original instance """
-        if lowleft < self.x < upright\
-        and lowleft < self.y < upright:
+        if lowleft[0] < self.x < upright[0]\
+        and lowleft[1] < self.y < upright[1]:
             return True
         else:
             return False
+        
+    def distance_from_point(self, x ,y )-> int:
+        """Determines the distance between x and y coordinates"""
+        distance = ((self.x -x)**2 + (self.y - y)**2) **0.5
+        return distance
+
+
 
 
 class House :
@@ -24,9 +31,13 @@ class Paint:
         self.buckets = buckets
         self.color = color
 
-Point1 = Point(10,11)
+Point1 = Point(10,20)
 
-print(Point1.fall_in_rectangle(3,12))
+print(Point1.fall_in_rectangle([10,12], [15,23]))
+
+print(Point(3,4).fall_in_rectangle([1,1], [6,6]))
+
+print(Point(3,4).distance_from_point(5,6))
 
 first_color = Paint(3, "blue")
 
