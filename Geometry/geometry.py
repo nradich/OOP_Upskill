c744d86e1bff08431914477cf3f2cd4cb44aae5a
avanickradich <nicholasradich1@gmail.com>
2023-12-11 14:48:04 -0800

    created a class calleld house

--- Geometry/geometry.py
+++ Geometry/geometry.py
@@ -3,3 +3,13 @@ class Point:
     def __init__(self, x, y):
         self.x =x
         self.y =y
+
+
+class House :
+    
+    def __init__(self, wall_area ):
+        self.wall_area = wall_area 
+
+Point1 = Point(10,20)
+
+print(type(Point1))
