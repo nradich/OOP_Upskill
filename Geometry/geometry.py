6eac1d8fc2cfee727396fad1a773befadf119cd8
avanickradich <nicholasradich1@gmail.com>
2023-12-11 16:14:35 -0800

    added total price method to paint class

--- Geometry/geometry.py
+++ Geometry/geometry.py
@@ -14,16 +14,21 @@ class Point:
         
     def distance_from_point(self, x ,y )-> int:
         """Determines the distance between x and y coordinates"""
-        distance = ((self.x -x)**2 + (self.y - y)**2) **0.5
+        distance = ((self.x -x)**2 + 
+                 (self.y - y)**2) **0.5
         return distance
 
 
 
-
 class House :
-    
+
     def __init__(self, wall_area ):
         self.wall_area = wall_area 
+
+    def paint_needed(self) -> int:
+        """Determines paint need for wall"""
+        need = self.wall_area * 2.5
+        return need 
 
 class Paint:
 
@@ -31,14 +36,25 @@ class Paint:
         self.buckets = buckets
         self.color = color
 
+    def total_price(self) -> float:
+        if self.color.lower() == "white":
+            price = 1.99
+        else:
+            price = 2.19
+        return price * self.buckets 
+
 Point1 = Point(10,20)
 
 print(Point1.fall_in_rectangle([10,12], [15,23]))
+Point2 = Point(8,9)
 
 print(Point(3,4).fall_in_rectangle([1,1], [6,6]))
 
-print(Point(3,4).distance_from_point(5,6))
+print(Point(3,4).distance_from_point(Point2.x,Point2.y))
 
-first_color = Paint(3, "blue")
+first_color = Paint(3, "white")
 
 print(first_color.buckets)
+
+print( House(4).paint_needed())
+print ( first_color.total_price())
