e4ab8dcf2e7f53f2d4ce8e6408180b57a09c10f6
avanickradich <nicholasradich1@gmail.com>
2023-12-12 12:18:19 -0800

    ability to print out both rectangles and points

--- Geometry/geo_inheritance.py
+++ Geometry/geo_inheritance.py
@@ -64,33 +64,36 @@ class GuiRectangle(Rectangle):
         canvas.left(90)
         canvas.forward(vertical_distance)
 
-        turtle.done()
-
         
 
-        
+class GuiPoint (Point):
+    """Child of point class"""
     
+    def draw(self, canvas, size =5, color="red"):
+        canvas.penup()
+        canvas.goto(self.x, self.y)
+        canvas.pendown()
+        canvas.dot(size, color )
+
+
 graph_rectangle = GuiRectangle(Point(randint(0, 400), randint(0, 400)), 
               Point(randint(10, 400), randint(10, 400)))
-
+# Print rectangle coordinates
+print("Rectangle Coordinates: ",
+      graph_rectangle.point1.x, ",",
+      graph_rectangle.point1.y, "and",
+      graph_rectangle.point2.x, ",",
+      graph_rectangle.point2.y)
+ 
+# Get point and area from user
+user_point = GuiPoint(float(input("Guess x: ")), float(input("Guess y: ")))
+user_area = float(input("Guess rectangle area: "))
+ 
+# Print out the game result
+print("Your point was inside rectangle: ", user_point.falls_in_rectangle(graph_rectangle))
+print("Your area was off by: ", graph_rectangle.area() - user_area)
 
 myturtle = turtle.Turtle()
-
-#transforming the rectangle to now be able to show in a graph 
 graph_rectangle.draw(canvas= myturtle)
-
-
-# # Print rectangle coordinates
-# print("Rectangle Coordinates: ",
-#       rectangle.point1.x, ",",
-#       rectangle.point1.y, "and",
-#       rectangle.point2.x, ",",
-#       rectangle.point2.y)
- 
-# # Get point and area from user
-# user_point = Point(float(input("Guess x: ")), float(input("Guess y: ")))
-# user_area = float(input("Guess rectangle area: "))
- 
-# # Print out the game result
-# print("Your point was inside rectangle: ", user_point.falls_in_rectangle(rectangle))
-# print("Your area was off by: ", rectangle.area() - user_area)
+user_point.draw(canvas= myturtle)
+turtle.done()
