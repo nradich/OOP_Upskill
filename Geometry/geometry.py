0adcc678ddf5b1c3e35af9113eb602c33318c1fa
avanickradich <nicholasradich1@gmail.com>
2023-12-11 16:41:44 -0800

    added area method to rectangle

--- Geometry/geometry.py
+++ Geometry/geometry.py
@@ -4,10 +4,20 @@ class Point:
         self.x =x
         self.y =y
 
-    def fall_in_rectangle(self, lowleft:list, upright:list) -> bool:
-        """Takes an input and compares vs the original instance """
+    def fall_in_rectangle_point(self, lowleft:list, upright:list) -> bool:
+        """Takes two points as  input and compares vs the original instance """
         if lowleft[0] < self.x < upright[0]\
         and lowleft[1] < self.y < upright[1]:
+            return True
+        else:
+            return False
+        
+    def fall_in_rectangle(self, rectangle) -> bool:
+        """Takes recetangle input and compares vs the original instance """
+        if rectangle.lowleft.x < self.x \
+        < rectangle.upright.x \
+        and rectangle.lowleft.y < self.y < \
+            rectangle.upright.y:
             return True
         else:
             return False
@@ -17,6 +27,21 @@ class Point:
         distance = ((self.x -x)**2 + 
                  (self.y - y)**2) **0.5
         return distance
+
+class Rectangle:
+
+    def __init__(self, lowleft:str, upright:str ) -> None:
+        """Take two points coordinates and store them as a rectangle """
+        self.lowleft = lowleft
+        self.upright = upright
+
+    def area(self) -> float:
+        """Calcualtes area of rectangle"""
+        area = (self.upright.x - self.lowleft.x ) \
+        * (self.upright.y - self.lowleft.y)
+
+        return area
+
 
 
 
@@ -45,10 +70,11 @@ class Paint:
 
 Point1 = Point(10,20)
 
-print(Point1.fall_in_rectangle([10,12], [15,23]))
+#print(Point1.fall_in_rectangle([10,12], [15,23]))
 Point2 = Point(8,9)
 
-print(Point(3,4).fall_in_rectangle([1,1], [6,6]))
+
+#print(Point(3,4).fall_in_rectangle([1,1], [6,6]))
 
 print(Point(3,4).distance_from_point(Point2.x,Point2.y))
 
@@ -58,3 +84,9 @@ print(first_color.buckets)
 
 print( House(4).paint_needed())
 print ( first_color.total_price())
+
+first_rectangle = Rectangle(Point(5,6), Point(7,9))
+
+first_rectangle = Rectangle(Point(1,1), Point(6,6))
+print(Point(3,4).fall_in_rectangle(first_rectangle))
+print(first_rectangle.area())
