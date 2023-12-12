dbf95235527ec4dbef761d5d25597cecb71306a2
avanickradich <nicholasradich1@gmail.com>
2023-12-12 11:00:48 -0800

    commented out user promot, using turtle package

--- Geometry/geo_inheritance.py
+++ Geometry/geo_inheritance.py
@@ -1,4 +1,13 @@
 import turtle 
+from random import randint
+
+
+myturtle = turtle.Turtle()
+
+myturtle.forward(12)
+
+
+
 class Point:
     """Example class to work through fundamentals/ Holds point inputs"""
     def __init__(self, x:int, y:int):
@@ -15,10 +24,10 @@ class Point:
         
     def fall_in_rectangle(self, rectangle) -> bool:
         """Takes recetangle input and compares vs the original instance """
-        if rectangle.lowleft.x < self.x \
-        < rectangle.upright.x \
-        and rectangle.lowleft.y < self.y < \
-            rectangle.upright.y:
+        if rectangle.point1.x < self.x \
+        < rectangle.point2.x \
+        and rectangle.point1.y < self.y < \
+            rectangle.point2.y:
             return True
         else:
             return False
@@ -31,14 +40,32 @@ class Point:
 
 class Rectangle:
 
-    def __init__(self, lowleft:str, upright:str ) -> None:
+    def __init__(self, point1:str, point2:str ) -> None:
         """Take two points coordinates and store them as a rectangle """
-        self.lowleft = lowleft
-        self.upright = upright
+        self.point1 = point1
+        self.point2 = point2
 
     def area(self) -> float:
         """Calcualtes area of rectangle"""
-        area = (self.upright.x - self.lowleft.x ) \
-        * (self.upright.y - self.lowleft.y)
+        area = (self.point2.x - self.point1.x ) \
+        * (self.point2.y - self.point1.y)
 
         return area
+    
+rectangle = Rectangle(Point(randint(0, 9), randint(0, 9)), 
+              Point(randint(10, 19), randint(10, 19)))
+ 
+# # Print rectangle coordinates
+# print("Rectangle Coordinates: ",
+#       rectangle.point1.x, ",",
+#       rectangle.point1.y, "and",
+#       rectangle.point2.x, ",",
+#       rectangle.point2.y)
+ 
+# # Get point and area from user
+# user_point = Point(float(input("Guess x: ")), float(input("Guess y: ")))
+# user_area = float(input("Guess rectangle area: "))
+ 
+# # Print out the game result
+# print("Your point was inside rectangle: ", user_point.fall_in_rectangle(rectangle))
+# print("Your area was off by: ", rectangle.area() - user_area)
